ad8e99ab790b57e3675b00922f65dc3be1c14922
ZikaZaki <17160061@su.edu.ye>
2024-02-16 02:19:03 +0300

    initial project setup

---
+++ src/test/java/curlerter/CurlerterTest.java
@@ -0,0 +1,21 @@
+package curlerter;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class CurlerterTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
